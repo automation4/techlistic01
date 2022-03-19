4d601716b89307fa9da267d06d11733559f96d96
himanshup4 <79011286+himanshup4@users.noreply.github.com>
2022-03-19 20:19:35 +0530

    second

--- src/test/java/com/techlistic/tests/ParameterTest.java
+++ src/test/java/com/techlistic/tests/ParameterTest.java
@@ -1,16 +1,22 @@
 package com.techlistic.tests;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Reporter;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class ParameterTest {
+    static final Logger log = LogManager.getLogger(PracticeForm.class.getName());
 
 @Parameters({"browser","version"})
     @Test(groups = {"smoke"})
     public void testParameter(String browser ,String version){
     Reporter.log("Browser is > "+browser);
+    log.info("Browser is > "+browser);
+
     Reporter.log("Version is > "+version);
+    log.info("Version is > "+version);
 
     }
 }
